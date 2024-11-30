a450f2d0f59cb69f58fb15024fa09e2fdfab78dc
Ogamdo <BellOfJerusalem@proton.me>
2024-11-30 23:57:19 +0900

    edit actionbuffers and reward, add observation

--- Runner.cs
+++ Runner.cs
@@ -5,10 +5,15 @@ using Unity.MLAgents.Sensors;
 
 public class Runner : Agent
 {
-    public Transform nemo;           // 네모(Nemo) 에이전트
-    public Transform goal;           // 목표 지점
-    public float speed = 100f;         // 이동 속도
-
+    public Transform runner;           // 네모(runner) 에이전트
+    public Transform lover;           // 도달해야할 연인
+    public float speed = 10f; // 이동 속도  
+    /*
+     현재는 lover의 위치가 고정 되어 있다. 만약에 lover가 더 빠른 속도로 움직이고, 에이전트가 현재의 보상을 깍으면 speed +=100도 방법일듯? */
+          
+    
+    [HideInInspector] public float dis= 0;
+ 
     public override void Initialize()
     {
         // 초기화 로직
@@ -16,63 +21,80 @@ public class Runner : Agent
 
     public override void OnEpisodeBegin()
     {
+       
         // 목표 위치를 X, Z 범위 -10에서 10 사이로 랜덤화
-             goal.localPosition = new Vector3(
+             lover.localPosition = new Vector3(
             Random.Range(-10f, 10f), // X축 범위
-            goal.localPosition.y,   // Y축 유지
+            lover.localPosition.y,   // Y축 유지
             Random.Range(-10f, 10f) // Z축 범위
+           
+
         );
+        dis = Vector3.Distance(lover.localPosition,runner.localPosition);
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         // 네모의 현재 위치를 관찰
-        sensor.AddObservation(nemo.localPosition);
+        sensor.AddObservation(runner.localPosition);
         // 목표의 현재 위치를 관찰
-        sensor.AddObservation(goal.localPosition);
+        sensor.AddObservation(lover.localPosition);
+        sensor.AddObservation(dis); //lover와 자신 사이의 거리를 관찰함
+        
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        // 행동 값 받기 (0: 움직이지 않음, 1: 전진, 2: 후진, 3: 왼쪽, 4: 오른쪽)
+        // 행동 값 받기 (0:전진, 1: 후진, 2: 좌, 3: 우)
         int action = actionBuffers.DiscreteActions[0];
 
         Vector3 move = Vector3.zero;
         switch (action)
         {
-            case 1: // 전진
+            case 0: // 전진
                 move = Vector3.forward * speed * Time.deltaTime;
                 break;
-            case 2: // 후진
+            case 1: // 후진
                 move = Vector3.back * speed * Time.deltaTime;
                 break;
-            case 3: // 왼쪽
+            case 2: // 왼쪽
                 move = Vector3.left * speed * Time.deltaTime;
                 break;
-            case 4: // 오른쪽
+            case 3: // 오른쪽
                 move = Vector3.right * speed * Time.deltaTime;
                 break;
         }
 
         // 이동 적용
-        nemo.localPosition += move;
+        runner.localPosition += move;
 
         // 목표 도달 여부 확인
-        if (Vector3.Distance(nemo.localPosition, goal.localPosition) < 1f)
+        if (dis < 1.0f)
         {
             SetReward(1f); // 목표 도달 시 보상
             EndEpisode(); // 에피소드 종료
         }
-
-        // 가만히 있는 경우 손실 부여
-        if (action == 0) // 행동이 "가만히 있음"일 경우
+        else if (1.0f<dis&&dis<3)
         {
-            AddReward(-0.25f); // 손실 추가
+           AddReward(0.1f);
+
         }
+        else if(dis>10)
+        {
+            AddReward(-1);
+        }
+        else
+        {
+           AddReward(-0.01f);
+        }
+      
+        
+        
         if(GetCumulativeReward() < 0.0f)
         {
             EndEpisode();
         }
+       
 
     }
 
@@ -80,11 +102,11 @@ public class Runner : Agent
     {
         // 수동 제어를 위한 로직
         var discreteActions = actionsOut.DiscreteActions;
-        discreteActions[0] = 0; // 기본값: 움직이지 않음
+      
 
-        if (Input.GetKey(KeyCode.W)) discreteActions[0] = 1; // 전진
-        if (Input.GetKey(KeyCode.S)) discreteActions[0] = 2; // 후진
-        if (Input.GetKey(KeyCode.A)) discreteActions[0] = 3; // 왼쪽
-        if (Input.GetKey(KeyCode.D)) discreteActions[0] = 4; // 오른쪽
+        if (Input.GetKey(KeyCode.W)) discreteActions[0] = 0; // 전진
+        if (Input.GetKey(KeyCode.S)) discreteActions[0] = 1; // 후진
+        if (Input.GetKey(KeyCode.A)) discreteActions[0] = 2; // 왼쪽
+        if (Input.GetKey(KeyCode.D)) discreteActions[0] = 3; // 오른쪽
     }
 }
